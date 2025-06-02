793742ba3ce9e2e332e3d1ee4e6f52a6c2d79869
Tiegow <tiegorafael@gmail.com>
2025-06-02 14:19:36 -0300

    Simulando banco de dados no backend
    Posts agora sao requisitados por fetch

--- public/js/pages/feed.js
+++ public/js/pages/feed.js
@@ -1,119 +1,22 @@
-window.addEventListener('DOMContentLoaded', () => {
-  loadRecipes();
+window.addEventListener('DOMContentLoaded', async () => {
+  await loadReceitas();
   loadComponent('components/cabecalho.html', 'header');
 });
 
-const posts = [
-  {
-    usuario: 'Ana_na_Cozinha',
-    titulo: 'Bolo de Cenoura com Cobertura de Chocolate',
-    imagem: 'https://conteudo.imguol.com.br/c/entretenimento/c8/2023/01/31/bolo-de-cenoura-low-carb-fit-1675176378739_v2_300x400.jpg',
-    data: '05/05/2025',
-    descricao: 'Um clássico bolo de cenoura fofinho com cobertura de chocolate. Abuvblws jadisjd aljduedjs fjsywofb wska se difena',
-    ingredientes: [
-    '1. Cenoura',
-    '2. Ovos',
-    '3. Açúcar',
-    '4. Óleo',
-    '5. Farinha de trigo',
-    '6. Fermento em pó',
-    '7. Chocolate em pó',
-    '8. Leite',
-    '9. Manteiga'
-    ]
-  },
-  {
-    usuario: 'Ana_na_Cozinha',
-    titulo: 'Pão de Queijo',
-    imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuBRLjSAIm48zNKn-zUKET-HjSRfAdTNehuQ&s',
-    data: '03/05/2025',
-    descricao: 'Receita mineira autêntica de pão de queijo crocante por fora, macio por dentro.',
-    ingredientes: [
-    '1. Polvilho doce',
-    '2. Queijo minas ralado',
-    ]
-  },
-  {
-    usuario: 'Ana_na_Cozinha',
-    titulo: 'Lasanha de Carne',
-    imagem: 'https://www.receiteria.com.br/wp-content/uploads/lasanha-de-carne-moida.jpeg',
-    data: '12/05/2025',
-    descricao: 'Receita de lasanha de carne com bastante queijo e sabor irresistível.',
-    ingredientes: [
-    '1. Massa para lasanha',
-    '2. Carne moída',
-    '3. Molho de tomate',
-    '4. Cebola',
-    '5. Alho',
-    '6. Queijo mussarela',
-    '7. Presunto',
-    '8. Sal',
-    '9. Orégano'
-    ]
-  },
-  {
-    usuario: 'Ana_na_Cozinha',
-    titulo: 'Bolo de Cenoura',
-    imagem: 'https://conteudo.imguol.com.br/c/entretenimento/c8/2023/01/31/bolo-de-cenoura-low-carb-fit-1675176378739_v2_300x400.jpg',
-    data: '05/05/2025',
-    descricao: 'Um clássico bolo de cenoura fofinho com cobertura de chocolate.',
-    ingredientes: [
-    '1. Cenoura',
-    '2. Ovos',
-    '3. Açúcar',
-    '4. Óleo',
-    '5. Farinha de trigo',
-    '6. Fermento em pó',
-    '7. Chocolate em pó',
-    '8. Leite',
-    '9. Manteiga'
-    ]
-  },
-  {
-    usuario: 'Ana_na_Cozinha',
-    titulo: 'Pão de Queijo',
-    imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuBRLjSAIm48zNKn-zUKET-HjSRfAdTNehuQ&s',
-    data: '03/05/2025',
-    descricao: 'Receita mineira autêntica de pão de queijo crocante por fora, macio por dentro.',
-    ingredientes: [
-    '1. Polvilho doce',
-    '2. Queijo minas ralado',
-    '3. Leite',
-    '4. Óleo',
-    '5. Ovos',
-    '6. Sal',
-    '7. Fermento em pó',
-    '8. Manteiga',
-    '9. Queijo parmesão'
-    ]
-  },
-  {
-    usuario: 'Ana_na_Cozinha',
-    titulo: 'Lasanha de Carne',
-    imagem: 'https://www.receiteria.com.br/wp-content/uploads/lasanha-de-carne-moida.jpeg',
-    data: '12/05/2025',
-    descricao: 'Receita de lasanha de carne com bastante queijo e sabor irresistível.',
-    ingredientes: [
-    '1. Massa para lasanha',
-    '2. Carne moída',
-    '3. Molho de tomate',
-    '4. Cebola',
-    '5. Alho',
-    '6. Queijo mussarela',
-    '7. Presunto',
-    '8. Sal',
-    '9. Orégano'
-    ]
-  },
-];
+async function loadReceitas() {
+  const response = await fetch('/api/feed');
+  const receitas = await response.json();
 
-async function loadRecipes() {
+  renderReceitas(receitas);
+}
+
+async function renderReceitas(receitas) {
   const response = await fetch('/components/cardReceita.html');
   const template = await response.text();
   const container = document.querySelector('#recipeList');
   container.innerHTML = '';
 
-  posts.forEach(receita => {
+  receitas.forEach(receita => {
     // Criar um elemento temporário com o template HTML
     const tempDiv = document.createElement('div');
     tempDiv.innerHTML = template;
--- routes.js
+++ routes.js
@@ -2,11 +2,7 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-// Simulação de login
-const USERS = [
-  { email: 'cookshare@gmail.com', password: '123456' },
-];
-
+// PAGINAS
 // Rota principal - Redireciona para a página de login
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -14,7 +10,6 @@ router.get('/', (req, res) => {
 
 router.get('/feed', (req,res) => {
   res.sendFile(path.join(__dirname, 'public', 'feed.html'));
-  console.log("entrou em feed");
 })
 
 router.get('/add-receita', (req,res) => {
@@ -25,6 +20,115 @@ router.get('/detalhes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'detalhes.html'));
 })
 
+// REST
+// Simulação de BD
+const posts = [
+  {
+    usuario: 'Ana_na_Cozinha',
+    titulo: 'Bolo de Cenoura com Cobertura de Chocolate',
+    imagem: 'https://conteudo.imguol.com.br/c/entretenimento/c8/2023/01/31/bolo-de-cenoura-low-carb-fit-1675176378739_v2_300x400.jpg',
+    data: '05/05/2025',
+    descricao: 'Um clássico bolo de cenoura fofinho com cobertura de chocolate.',
+    ingredientes: [
+    '1. Cenoura',
+    '2. Ovos',
+    '3. Açúcar',
+    '4. Óleo',
+    '5. Farinha de trigo',
+    '6. Fermento em pó',
+    '7. Chocolate em pó',
+    '8. Leite',
+    '9. Manteiga'
+    ]
+  },
+  {
+    usuario: 'Ana_na_Cozinha',
+    titulo: 'Pão de Queijo',
+    imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuBRLjSAIm48zNKn-zUKET-HjSRfAdTNehuQ&s',
+    data: '03/05/2025',
+    descricao: 'Receita mineira autêntica de pão de queijo crocante por fora, macio por dentro.',
+    ingredientes: [
+    '1. Polvilho doce',
+    '2. Queijo minas ralado',
+    ]
+  },
+  {
+    usuario: 'Ana_na_Cozinha',
+    titulo: 'Lasanha de Carne',
+    imagem: 'https://www.receiteria.com.br/wp-content/uploads/lasanha-de-carne-moida.jpeg',
+    data: '12/05/2025',
+    descricao: 'Receita de lasanha de carne com bastante queijo e sabor irresistível.',
+    ingredientes: [
+    '1. Massa para lasanha',
+    '2. Carne moída',
+    '3. Molho de tomate',
+    '4. Cebola',
+    '5. Alho',
+    '6. Queijo mussarela',
+    '7. Presunto',
+    '8. Sal',
+    '9. Orégano'
+    ]
+  },
+  {
+    usuario: 'Ana_na_Cozinha',
+    titulo: 'Bolo de Cenoura',
+    imagem: 'https://conteudo.imguol.com.br/c/entretenimento/c8/2023/01/31/bolo-de-cenoura-low-carb-fit-1675176378739_v2_300x400.jpg',
+    data: '05/05/2025',
+    descricao: 'Um clássico bolo de cenoura fofinho com cobertura de chocolate.',
+    ingredientes: [
+    '1. Cenoura',
+    '2. Ovos',
+    '3. Açúcar',
+    '4. Óleo',
+    '5. Farinha de trigo',
+    '6. Fermento em pó',
+    '7. Chocolate em pó',
+    '8. Leite',
+    '9. Manteiga'
+    ]
+  },
+  {
+    usuario: 'Ana_na_Cozinha',
+    titulo: 'Pão de Queijo',
+    imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuBRLjSAIm48zNKn-zUKET-HjSRfAdTNehuQ&s',
+    data: '03/05/2025',
+    descricao: 'Receita mineira autêntica de pão de queijo crocante por fora, macio por dentro.',
+    ingredientes: [
+    '1. Polvilho doce',
+    '2. Queijo minas ralado',
+    '3. Leite',
+    '4. Óleo',
+    '5. Ovos',
+    '6. Sal',
+    '7. Fermento em pó',
+    '8. Manteiga',
+    '9. Queijo parmesão'
+    ]
+  },
+  {
+    usuario: 'Ana_na_Cozinha',
+    titulo: 'Lasanha de Carne',
+    imagem: 'https://www.receiteria.com.br/wp-content/uploads/lasanha-de-carne-moida.jpeg',
+    data: '12/05/2025',
+    descricao: 'Receita de lasanha de carne com bastante queijo e sabor irresistível.',
+    ingredientes: [
+    '1. Massa para lasanha',
+    '2. Carne moída',
+    '3. Molho de tomate',
+    '4. Cebola',
+    '5. Alho',
+    '6. Queijo mussarela',
+    '7. Presunto',
+    '8. Sal',
+    '9. Orégano'
+    ]
+  },
+];
+// Simulação de login
+const USERS = [
+  { email: 'cookshare@gmail.com', password: '123456' },
+];
 router.post('/login', (req, res) => {
   const { email, password } = req.body;
   const user = USERS.find(u => u.email === email && u.password === password);
@@ -50,5 +154,9 @@ router.post('/register', (req, res) => {
   USERS.push({ email, username, password });
   res.sendStatus(200);
 });
+
+router.get('/api/feed', (req, res) => {
+  res.json(posts);
+})
 
 module.exports = router;
